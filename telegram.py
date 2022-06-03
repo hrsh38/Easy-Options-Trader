ac71a9eb48024663fb5e085dda75477975830a25
Harsh Patel <harshp97@gmail.com>
2022-06-02 22:31:11 -0400

    Pulling from telegram

--- telegram.py
+++ telegram.py
@@ -1,6 +1,26 @@
+import configparser
 import json
+import re
+import asyncio
+from telethon.errors import SessionPasswordNeededError
+from telethon import TelegramClient, events, sync
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import (
+PeerChannel
+)
 
-items = json.loads('[{"id" : 1},{"id" : 2}]')
+api_id = "12510265"
+api_hash = "9a2309a0a34b0c8d9eee2ce409e4a30a"
 
-for item in items:
-    print(item["id"])
+user_input_channel = "https://t.me/testingtestertest"
+
+client = TelegramClient('Test', api_id, api_hash).start()
+
+@client.on(events.NewMessage())
+async def newMessage(event):
+    newMessage = event.message.message
+    print(newMessage)
+
+with client:
+    client.run_until_disconnected()
+
